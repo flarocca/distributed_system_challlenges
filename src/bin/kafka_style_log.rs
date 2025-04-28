669def2f4f223af537db7389d1d1d35e53e70d84
Facundo La Rocca <facu.larocca@gmail.com>
2025-04-28 10:11:53 -0300

    Fixed multi-node mode

--- src/bin/kafka_style_log.rs
+++ src/bin/kafka_style_log.rs
@@ -1,11 +1,12 @@
 use anyhow::Context;
 use distributed_system_challenges::{main_loop, Body, Message, Node};
 use redis::{Commands, Connection};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer};
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     io::{StdoutLock, Write},
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, MutexGuard},
+    time::Duration,
 };
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -22,31 +23,36 @@ enum Payload {
         msg: usize,
     },
     SendOk {
-        offset: usize,
+        offset: Offset,
     },
     Poll {
-        offsets: HashMap<String, usize>,
+        offsets: HashMap<String, Offset>,
     },
     PollOk {
-        msgs: HashMap<String, Vec<Vec<usize>>>,
+        #[serde(serialize_with = "serialize_as_pairs")]
+        msgs: HashMap<String, HashMap<Offset, usize>>,
     },
     CommitOffsets {
-        offsets: HashMap<String, usize>,
+        offsets: HashMap<String, Offset>,
     },
     CommitOffsetsOk,
     ListCommittedOffsets {
         keys: Vec<String>,
     },
     ListCommittedOffsetsOk {
-        offsets: HashMap<String, usize>,
+        offsets: HashMap<String, Offset>,
     },
     TriggerGossip,
     Gossip {
         seen: SeenLogs,
     },
+    Anchor,
 }
 
-type Logs = HashMap<String, Vec<LogEntry>>;
+type NodeId = String;
+type KeyId = String;
+type Offset = usize;
+type Logs = HashMap<KeyId, Vec<LogEntry>>;
 
 // TODO: we don't need to duplicate the whole structure, we could
 // just keep messages ids seen by other nodes which will reduce
@@ -60,20 +66,25 @@ struct SeenLogs {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct LogEntry {
     msg_id: usize,
-    key: String,
-    offset: usize,
+    key: KeyId,
+    offset: Offset,
     msg: usize,
+    seen_by: HashSet<NodeId>,
 }
 
 struct KafkaStyleLogNode {
-    node_id: String,
+    node_id: NodeId,
     message_id: usize,
-    neighbors: Vec<String>,
-    known: HashMap<String, SeenLogs>,
+    neighbors: HashSet<NodeId>,
+    known: HashMap<NodeId, SeenLogs>,
     connection: Arc<Mutex<Connection>>,
+    gossip_interval: Duration,
+    gossip_batch_size: usize,
     // Node specific state
     uncommitted_logs: Arc<Mutex<Logs>>,
     committed_logs: Arc<Mutex<Logs>>,
+    anchored_uncommitted_logs: Arc<Mutex<Logs>>,
+    anchored_committed_logs: Arc<Mutex<Logs>>,
 }
 
 impl KafkaStyleLogNode {
@@ -81,17 +92,21 @@ impl KafkaStyleLogNode {
         Self {
             node_id: "uninit".to_owned(),
             message_id: 0,
-            neighbors: Vec::new(),
+            neighbors: HashSet::new(),
             known: HashMap::new(),
             connection,
+            gossip_interval: Duration::from_millis(300),
+            gossip_batch_size: 100,
             uncommitted_logs: Arc::new(Mutex::new(HashMap::new())),
             committed_logs: Arc::new(Mutex::new(HashMap::new())),
+            anchored_uncommitted_logs: Arc::new(Mutex::new(HashMap::new())),
+            anchored_committed_logs: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
     fn send_message<T>(&mut self, stdout: &mut StdoutLock, message: &T) -> anyhow::Result<()>
     where
-        T: ?Sized + Serialize,
+        T: ?Sized + Serialize + std::fmt::Debug,
     {
         serde_json::to_writer(&mut *stdout, message).context("Error serializing response")?;
         stdout
@@ -132,7 +147,7 @@ impl KafkaStyleLogNode {
             .iter()
             .filter(|n| *n != node_id)
             .map(|n| n.to_owned())
-            .collect::<Vec<_>>();
+            .collect::<HashSet<_>>();
 
         self.known
             .extend(nodes.iter().map(|id| (id.clone(), SeenLogs::default())));
@@ -158,18 +173,21 @@ impl KafkaStyleLogNode {
         // it should return the actual offset
         let cloned = self.uncommitted_logs.clone();
         let mut uncommitted_logs = cloned.lock().unwrap();
-        //let offset = self.offset.load(Ordering::Relaxed);
+
         let offset = self
             .connection
             .lock()
             .unwrap()
             .incr(format!("{key}::offset"), 1)?;
 
+        let mut seen_by = HashSet::new();
+        seen_by.insert(self.node_id.to_owned());
         let log_entry = LogEntry {
             msg_id: self.message_id,
             key: key.to_owned(),
             offset,
             msg,
+            seen_by,
         };
 
         uncommitted_logs
@@ -198,8 +216,8 @@ impl KafkaStyleLogNode {
     ) -> anyhow::Result<()> {
         // TODO: non existing keys are ignored. This might not be the best
         // alternative. Maybe an empty vector should be returned instead
-        let committed_cloned = self.committed_logs.clone();
-        let uncommitted_cloned = self.uncommitted_logs.clone();
+        let committed_cloned = self.anchored_committed_logs.clone();
+        let uncommitted_cloned = self.anchored_uncommitted_logs.clone();
 
         let committed_logs = committed_cloned.lock().unwrap();
         let uncommitted_logs = uncommitted_cloned.lock().unwrap();
@@ -207,7 +225,7 @@ impl KafkaStyleLogNode {
         let mut msgs = HashMap::new();
 
         for (key, offset) in &offsets {
-            let mut log_entries = Vec::new();
+            let mut log_entries = HashMap::new();
 
             if committed_logs.contains_key(key) {
                 let log = committed_logs.get(key).expect("Key not found");
@@ -215,8 +233,8 @@ impl KafkaStyleLogNode {
                 let logs = log
                     .iter()
                     .filter(|l| l.offset >= *offset)
-                    .map(|l| vec![l.offset, l.msg])
-                    .collect::<Vec<_>>();
+                    .map(|l| (l.offset, l.msg))
+                    .collect::<HashMap<_, _>>();
 
                 log_entries.extend(logs.clone());
             }
@@ -227,8 +245,8 @@ impl KafkaStyleLogNode {
                 let logs = log
                     .iter()
                     .filter(|l| l.offset >= *offset)
-                    .map(|l| vec![l.offset, l.msg])
-                    .collect::<Vec<_>>();
+                    .map(|l| (l.offset, l.msg))
+                    .collect::<HashMap<_, _>>();
 
                 log_entries.extend(logs.clone());
             }
@@ -295,7 +313,7 @@ impl KafkaStyleLogNode {
     fn handle_list_committed_offsets(
         &mut self,
         message: &Message<Payload>,
-        keys: &Vec<String>,
+        keys: &Vec<KeyId>,
         stdout: &mut StdoutLock,
     ) -> anyhow::Result<()> {
         let committed_cloned = self.committed_logs.clone();
@@ -325,7 +343,7 @@ impl KafkaStyleLogNode {
         self.send_message(stdout, &reply)
     }
 
-    fn handle_gossip(&mut self, src: &str, seen: SeenLogs) {
+    fn handle_gossip(&mut self, src: &str, seen: SeenLogs) -> anyhow::Result<()> {
         let node = self.known.get_mut(src).expect("Unknown node");
 
         let committed_cloned = self.committed_logs.clone();
@@ -339,21 +357,29 @@ impl KafkaStyleLogNode {
 
         for (key, entries) in &seen.committed {
             for entry in entries {
+                let mut new_entry = entry.clone();
+                new_entry.seen_by.insert(self.node_id.to_owned());
+
                 committed_logs
                     .entry(key.clone())
                     .or_default()
-                    .push(entry.clone());
+                    .push(new_entry);
             }
         }
 
         for (key, entries) in &seen.uncommitted {
             for entry in entries {
+                let mut new_entry = entry.clone();
+                new_entry.seen_by.insert(self.node_id.to_owned());
+
                 uncommitted_logs
                     .entry(key.clone())
                     .or_default()
-                    .push(entry.clone());
+                    .push(new_entry);
             }
         }
+
+        self.anchor_messages(&mut uncommitted_logs)
     }
 
     fn handle_trigger_gossip(&mut self, stdout: &mut StdoutLock) -> anyhow::Result<()> {
@@ -364,72 +390,162 @@ impl KafkaStyleLogNode {
         let committed_cloned = self.committed_logs.clone();
         let uncommitted_cloned = self.uncommitted_logs.clone();
 
-        let committed_logs = committed_cloned.lock().unwrap();
-        let uncommitted_logs = uncommitted_cloned.lock().unwrap();
+        let mut committed_logs = committed_cloned.lock().unwrap();
+        let mut uncommitted_logs = uncommitted_cloned.lock().unwrap();
 
-        let messages = self
-            .neighbors
-            .iter()
-            .map(|n| {
-                let mut n_not_seen = SeenLogs::default();
+        let mut messages = Vec::new();
 
-                for (key, value) in committed_logs.iter() {
-                    for entry in value {
-                        if !self
+        for neighbor in &self.neighbors {
+            let mut n_not_seen = SeenLogs::default();
+
+            for (key, value) in committed_logs.iter() {
+                for entry in value {
+                    if !self.known.contains_key(neighbor)
+                        || !self
                             .known
-                            .get(n)
+                            .get(neighbor)
                             .expect("Unknown node")
                             .committed
                             .contains_key(key)
-                        {
-                            n_not_seen
-                                .committed
-                                .entry(key.clone())
-                                .or_default()
-                                .push(entry.clone());
-                        }
+                    {
+                        n_not_seen
+                            .committed
+                            .entry(key.clone())
+                            .or_default()
+                            .push(entry.clone());
+                    }
+
+                    if n_not_seen.committed.len() == self.gossip_batch_size {
+                        break;
                     }
                 }
 
-                for (key, value) in uncommitted_logs.iter() {
-                    for entry in value {
-                        if !self
+                if n_not_seen.committed.len() == self.gossip_batch_size {
+                    break;
+                }
+            }
+
+            for (key, value) in uncommitted_logs.iter() {
+                for entry in value {
+                    if !self.known.contains_key(key)
+                        || !self
                             .known
-                            .get(n)
+                            .get(neighbor)
                             .expect("Unknown node")
                             .uncommitted
                             .contains_key(key)
-                        {
-                            n_not_seen
-                                .uncommitted
-                                .entry(key.clone())
-                                .or_default()
-                                .push(entry.clone());
-                        }
+                    {
+                        n_not_seen
+                            .uncommitted
+                            .entry(key.clone())
+                            .or_default()
+                            .push(entry.clone());
+                    }
+
+                    if n_not_seen.uncommitted.len() == self.gossip_batch_size {
+                        break;
                     }
                 }
 
-                Message::new(
-                    self.node_id.to_owned(),
-                    n.to_owned(),
-                    Body::new(
-                        Some(self.message_id),
-                        None,
-                        Payload::Gossip { seen: n_not_seen },
-                    ),
-                )
-            })
+                if n_not_seen.uncommitted.len() == self.gossip_batch_size {
+                    break;
+                }
+            }
+
+            messages.push((neighbor.to_owned(), n_not_seen));
+        }
+
+        let seen_logs = messages
+            .iter()
+            .filter(|(_, seen)| !seen.uncommitted.is_empty() || !seen.committed.is_empty())
             .collect::<Vec<_>>();
 
-        self.send_messages(stdout, &messages)
+        let mut messages = Vec::new();
+
+        for (neighbor, n_not_seen) in &seen_logs {
+            let known = self.known.get_mut(neighbor).expect("Unknown node");
+            known.committed.extend(n_not_seen.committed.clone());
+            known.uncommitted.extend(n_not_seen.uncommitted.clone());
+
+            messages.push(Message::new(
+                self.node_id.to_owned(),
+                neighbor.to_owned(),
+                Body::new(
+                    Some(self.message_id),
+                    None,
+                    Payload::Gossip {
+                        seen: n_not_seen.clone(),
+                    },
+                ),
+            ));
+        }
+
+        for (neighbor, n_not_seen) in &seen_logs {
+            for seen in n_not_seen.uncommitted.iter() {
+                for c in seen.1.iter() {
+                    let entries = uncommitted_logs.get_mut(seen.0).expect("Invalid key");
+                    if let Some(entry) = entries.iter_mut().find(|e| e.msg_id == c.msg_id) {
+                        entry.seen_by.insert(neighbor.to_owned());
+                    }
+                }
+            }
+
+            for seen in n_not_seen.committed.iter() {
+                for c in seen.1.iter() {
+                    let entries = committed_logs.get_mut(seen.0).expect("Invalid key");
+                    if let Some(entry) = entries.iter_mut().find(|e| e.msg_id == c.msg_id) {
+                        entry.seen_by.insert(neighbor.to_owned());
+                    }
+                }
+            }
+        }
+
+        if !messages.is_empty() {
+            return self.send_messages(stdout, &messages);
+        }
+
+        Ok(())
+    }
+
+    fn handle_anchor(&mut self) -> anyhow::Result<()> {
+        let uncommitted_cloned = self.uncommitted_logs.clone();
+        let mut uncommitted_logs = uncommitted_cloned.lock().unwrap();
+
+        let result = self.anchor_messages(&mut uncommitted_logs);
+
+        result
+    }
+
+    fn anchor_messages(&mut self, uncommitted_logs: &mut MutexGuard<Logs>) -> anyhow::Result<()> {
+        let mut anchored_uncommitted_logs = self.anchored_uncommitted_logs.lock().unwrap();
+
+        let mut cluster = self.neighbors.clone();
+        cluster.insert(self.node_id.to_owned());
+
+        for (key, entries) in uncommitted_logs.iter_mut() {
+            let anchor_entries = entries
+                .iter()
+                .filter(|entry| entry.seen_by == cluster)
+                .cloned()
+                .collect::<Vec<_>>();
+
+            entries.retain(|entry| entry.seen_by != cluster);
+            anchored_uncommitted_logs
+                .entry(key.to_owned())
+                .or_default()
+                .extend(anchor_entries);
+        }
+
+        Ok(())
     }
 }
 
 impl Node<Payload> for KafkaStyleLogNode {
     fn init(&mut self, tx: std::sync::mpsc::Sender<Message<Payload>>) -> anyhow::Result<()> {
         let node_id = self.node_id.clone();
+        let gossip_interval = self.gossip_interval;
         let _ = std::thread::spawn(move || loop {
-            std::thread::sleep(std::time::Duration::from_millis(300));
+            std::thread::sleep(gossip_interval);
 
             let trigger_gossip = Message::<Payload>::new(
                 node_id.clone(),
@@ -438,6 +554,16 @@ impl Node<Payload> for KafkaStyleLogNode {
             );
 
             if tx.send(trigger_gossip).is_err() {
+                break;
+            }
+
+            let anchor = Message::<Payload>::new(
+                node_id.clone(),
+                node_id.clone(),
+                Body::new(None, None, Payload::Anchor),
+            );
+
+            if tx.send(anchor).is_err() {
                 break;
             }
         });
@@ -469,7 +595,8 @@ impl Node<Payload> for KafkaStyleLogNode {
             }
             Payload::ListCommittedOffsetsOk { .. } => {}
             Payload::TriggerGossip => self.handle_trigger_gossip(stdout)?,
-            Payload::Gossip { seen } => self.handle_gossip(message.src(), seen.clone()),
+            Payload::Gossip { seen } => self.handle_gossip(message.src(), seen.clone())?,
+            Payload::Anchor => self.handle_anchor()?,
         };
 
         Ok(())
@@ -486,3 +613,60 @@ fn main() -> anyhow::Result<()> {
     let mut node = KafkaStyleLogNode::new(connection);
     main_loop::<Message<Payload>, _, Payload>(&mut node)
 }
+
+fn serialize_as_pairs<S>(
+    msgs: &HashMap<String, HashMap<Offset, usize>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let result = msgs
+        .iter()
+        .map(|(key, entries)| {
+            let mut pairs = entries
+                .iter()
+                .map(|(offset, message)| (*offset, *message))
+                .collect::<Vec<_>>();
+
+            pairs.sort_by_key(|(k, _)| *k);
+
+            let pairs = pairs.iter().map(|(k, v)| vec![*k, *v]).collect();
+
+            (key.to_owned(), pairs)
+        })
+        .collect::<HashMap<String, Vec<Vec<_>>>>();
+
+    let json = serde_json::value::to_value(&result).expect("Error deserializing HashMap");
+
+    json.serialize(serializer)
+}
+
+//#[cfg(test)]
+//mod tests {
+//    use std::{collections::HashMap, io::Write};
+//
+//    use crate::Payload;
+//
+//    #[test]
+//    fn test_serializer() {
+//        let mut stdout = std::io::stdout().lock();
+//
+//        let mut entries = HashMap::new();
+//        entries.insert(1, 10);
+//        entries.insert(2, 11);
+//        entries.insert(9, 123);
+//        entries.insert(5, 9786);
+//        entries.insert(6, 1);
+//        let mut msgs = HashMap::new();
+//        msgs.insert(1.to_string(), entries);
+//
+//        let payload = Payload::PollOk { msgs };
+//
+//        serde_json::to_writer(&mut stdout, &payload).expect("Error serializing response");
+//
+//        stdout
+//            .write_all(b"\n")
+//            .expect("Error writing response to stdout");
+//    }
+//}
